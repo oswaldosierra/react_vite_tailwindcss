5562e3dbfe10c125f2fd1d6c69818e0f1e6f7643
Oswaldo Luis Sierra Bran <oswaldox199@gmail.com>
2023-09-15 23:13:32 -0500

    Se comienza a crear el detalle de la orden

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,12 @@
+const ProductDetail = () => {
+  return (
+    <aside className="product-detail flex fixed flex-col right-0 border border-black rounded-lg bg-white top-[68px] w-[360px] h-[calc(100vh-80px)] z-20 shadow-lg">
+      <div className="flex justify-between items-center px-2">
+        <h2>Detail</h2>
+        <span>x</span>
+      </div>
+    </aside>
+  );
+};
+
+export default ProductDetail;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Card from "../../Components/Card";
 import Layout from "../../Components/Layout/Index";
+import ProductDetail from "../../Components/ProductDetail";
 
 function Home() {
   const [items, setItems] = useState([]);
@@ -14,6 +15,7 @@ function Home() {
   return (
     <Layout>
       Home
+      <ProductDetail />
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
         {items.map((item) => (
           <Card key={item.id} data={item} />
