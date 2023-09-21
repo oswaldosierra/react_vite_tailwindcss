cbcc4002289abe69681b3a3cf6e5ca910b93ed4e
Oswaldo Luis Sierra Bran <oswaldox199@gmail.com>
2023-09-20 22:03:01 -0500

    Se crea la card de los objetos del carrito y se visualizan en el mismo

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../OrderCard";
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
@@ -11,12 +12,22 @@ const CheckoutSideMenu = () => {
         context.isCheckoutSideMenuOpen ? "flex" : "hidden"
       } product-detail fixed flex-col right-0 border border-black rounded-lg bg-white top-[68px] w-[360px] h-[calc(100vh-80px)] z-20 shadow-lg`}
     >
-      <div className="flex justify-between items-center px-3 py-3">
+      <div className="flex justify-between items-center p-6">
         <h2>My Order</h2>
         <XMarkIcon
           onClick={context.closeCheckoutSideMenu}
           className="h-6 w-6 text-black cursor-pointer"
         />
+      </div>
+      <div className="px-6">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
